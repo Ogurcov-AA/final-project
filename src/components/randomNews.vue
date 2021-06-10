f12d45859c0ee0fe65654b679b2468618d1cdfc8
Sasha Ogurtsov <sanekLLIypa@yandex.ru>
2021-06-10 13:59:12 +0300

    feat: add timer for random news

--- src/components/randomNews.vue
+++ src/components/randomNews.vue
@@ -1,7 +1,9 @@
 <template>
-<div>
-    <news/>
- </div>
+  <div>
+    <news v-bind:title="randNews.title"
+          v-bind:description="randNews.description"
+          v-bind:url="randNews.urlToImage"/>
+  </div>
 </template>
 
 <script>
@@ -9,8 +11,37 @@ import news from "@/components/news";
 
 export default {
   name: "randomNews",
-  components:{
+  components: {
     news
+  },
+  data() {
+    return {
+      newsList: '',
+      randNews: ''
+    }
+  },
+  created() {
+    this.getRandomNews()
+    this.timer()
+  },
+  methods: {
+    timer(){
+      setInterval(this.getRandomNews,30000)
+    },
+    getRandomNews(){
+      this.getList()
+      this.randNews = this.random(this.newsList)
+    },
+    getList() {
+      this.newsList = this.$store.getters.getNewsList
+    },
+    random(arr) {
+      let key = this.getRandomInt(0, arr.length - 1);
+      return arr[key];
+    },
+    getRandomInt(min, max) {
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
   }
 }
 </script>
